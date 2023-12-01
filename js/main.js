423de73e2e8eee8b197f06fafad07798b3e38506
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2023-12-01 17:15:31 +0200

    Mobile Layout of the main page

--- js/main.js
+++ js/main.js
@@ -1,26 +1,52 @@
-document.addEventListener( 'DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let splide = new Splide('.js-main-slider', {
         perPage: 3,
         rewind: true,
     });
-    // splide.on( 'resize', function ( isOverflow ) {
-    //     splide.options = {
-    //         perPage: 2,
-    //     };
-    // } );
+    splide.on('resize', function () {
+        let perPage = $(window).width() > 1383 ? 3 : 2
+        if ($(window).width() < 558) {
+            perPage = 1;
+        }
+        splide.options = {
+            perPage: perPage,
+        };
+    });
     splide.mount();
+
     let attractionSlider = new Splide('.js-attraction-slider', {
-        perPage: 4,
+        perPage: perPageCount(),
         rewind: true,
+    });
+    attractionSlider.on('resize', function () {
+        attractionSlider.options = {
+            perPage: perPageCount(),
+        };
     });
     attractionSlider.mount();
 
     let foodSlider = new Splide('.js-food-slider', {
-        perPage: 4,
+        perPage: perPageCount(),
         rewind: true,
+    });
+    foodSlider.on('resize', function () {
+        foodSlider.options = {
+            perPage: perPageCount(),
+        };
     });
     foodSlider.mount();
 });
+
+let perPageCount = function () {
+    let perPage = $(window).width() > 1383 ? 4 : 3;
+    if ($(window).width() < 974) {
+        perPage = 2;
+    }
+    if ($(window).width() < 750) {
+        perPage = 1;
+    }
+    return perPage;
+}
 
 $('#menu-up-control').click(function () {
     let menuGeneral = $('#menu-general');
