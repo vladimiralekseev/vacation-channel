01fde870ebbe06adbaab74c2133d1f76413ccc92
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2024-01-24 22:04:09 +0200

    Edit the programming page

--- frontend/views/programming/index.php
+++ frontend/views/programming/index.php
@@ -1,7 +1,6 @@
 <div class="programming-list">
     <div class="item">
         <h2>Children's Programming</h2>
-        <div>Special programs exclusively for children aged 6 to 16 are broadcasted every day from 3:00 PM to 4:00 PM.</div>
         <div>Children's programming airs daily, in the segment titled Kids Connection, on the Vacation Channel.</div>
     </div>
 </div>
