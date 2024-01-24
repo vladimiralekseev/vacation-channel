187b4c9e35756e6ba2d070e1af6008fcbc63215d
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2024-01-24 21:32:18 +0200

    Edit the programming page

--- frontend/views/programming/index.php
+++ frontend/views/programming/index.php
@@ -1,15 +1,5 @@
 <div class="programming-list">
     <div class="item">
-        <h2>Branson TV Guide</h2>
-        <div>Visitor-oriented news answers the most frequently asked questions from visitors 24 hours a day.
-        </div>
-    </div>
-    <div class="item">
-        <h2>Weather</h2>
-        <div>We keep visitors and the community updated on local weather throughout the day. Weather summaries are provided every 30 minutes.
-        </div>
-    </div>
-    <div class="item">
         <h2>Children's Programming</h2>
         <div>Special programs exclusively for children aged 6 to 16 are broadcasted every day from 3:00 PM to 4:00 PM.</div>
         <div>Children's programming airs daily, in the segment titled Kids Connection, on the Vacation Channel.</div>
